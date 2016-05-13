6941f5a7f9ba880f6b39a596aafaf8592a8d19b4
John McLevey <mclevey@me.com>
2016-05-13 11:16:35 -0400

    removed copy script and added bash script to automate new website workflow

--- copy_to_site.py
+++
@@ -1,6 +0,0 @@
-import shutil
-
-shutil.copy("/Users/johnmclevey/Documents/cv/cv_mclevey.pdf", "/Users/johnmclevey/Documents/websites/john/source_mclevey/content/pdfs/cv_mclevey.pdf")
-# '/Users/johnmclevey/Dropbox/websites/mclevey.github.io/cv_mclevey.pdf'
-
-print("cv copied to /Users/johnmclevey/Documents/websites/john/source_mclevey/content/pdfs/. compile website with hugo, move over public content, commit and push.")
