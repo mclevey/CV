ce97d47d4d859f482f80651ee239fed34e65fca4
John McLevey <john.mclevey@uwaterloo.ca>
2023-04-21 18:22:32 -0230

    quick update

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pandoc -r markdown+simple_tables+table_captions+yaml_metadata_block+smart --pdf-engine=xelatex --template=cv.tex cv_mclevey.md -o cv_mclevey.pdf
-
-git add . && git commit -m 'routine update' && git push
