3f5bca8437d6e8b8126e271617f98b15f943baae
John McLevey <john.mclevey@hey.com>
2024-03-02 09:40:50 -0330

    routine cv update

--- compile_cv.sh
+++ compile_cv.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-echo -e "\033[0;32mProducing overview tables...\033[0m"
+echo -e ✅ Producing overview tables
 python src/produce-overview-tables.py
 
-echo -e "\033[0;32mCompiling CV...\033[0m"
+echo -e ✅ Compiling CV
 pandoc cv.md -o cv.pdf --template=templates_and_tables/template-cv.tex --pdf-engine=xelatex
 
-echo -e "\033[0;32mCopying files to mun-application...\033[0m"
-cp cv.pdf /Users/johnmclevey/Vaults/EC2/mun-application/_application/
-
-echo -e "\033[0;32mPushing to git repo...\033[0m"
+echo -e ✅ Pushing to git repo
 git add . && git commit -m 'routine cv update' && git push
 
-echo -e "\033[0;32mDone.\033[0m"
-
+echo -e ✅ Copying files to mun-application
+cp cv.pdf /Users/johnmclevey/Vaults/EC2/mun-application/_application/
