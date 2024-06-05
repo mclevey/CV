46229f00be8e453bb1a216644927215e23fc0217
John McLevey <john.mclevey@hey.com>
2024-06-05 08:16:23 -0230

    routine cv update

--- compile_cv.sh
+++ compile_cv.sh
@@ -5,6 +5,7 @@ python src/produce-overview-tables.py
 
 echo -e ✅ COMPILING CV
 pandoc cv.md -o cv.pdf --template=templates_and_tables/template-cv.tex --pdf-engine=xelatex
+pandoc cv.md -o cv.tex --template=templates_and_tables/template-cv.tex --pdf-engine=xelatex
 
 echo -e ✅ PUSHING TO GIT REPO
 git add . && git commit -m 'routine cv update' && git push
