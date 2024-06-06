f65a81aa0afe6aed8f01394e70afb05506c599dc
John McLevey <john.mclevey@hey.com>
2024-06-06 14:21:28 -0230

    routine cv update

--- compile_cv.sh
+++ compile_cv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e 
+set -e 	
 
 echo -e "✅ PRODUCING OVERVIEW TABLES"
 python src/produce-overview-tables.py
@@ -10,7 +10,7 @@ pandoc cv.md -o cv.pdf --template=templates_and_tables/template-cv.tex --pdf-eng
 pandoc cv.md -o cv-web.md --template=templates_and_tables/template-cv.md
 
 echo -e "✅ COPYING CV (HTML) TO GITHUB WEBSITE REPO"
-mv cv-web.md ~/Vaults/EC2/websites/mclevey.github.io-source/pages/cv-web.qmd
+mv cv-web.md ~/Vaults/EC2/websites/mclevey.github.io-source/pages/cv.qmd
 
 echo -e "✅ PUSHING CV GITHUB REPOS"
 git add . && git commit -m 'routine cv update' && git push
@@ -21,7 +21,7 @@ echo -e "✅ COPYING SITE FILES TO PUBLIC DIRECTORY"
 cp -r ~/Vaults/EC2/websites/mclevey.github.io-source/_site/* ~/Vaults/EC2/websites/mclevey.github.io
 
 echo -e "✅ PUSHING WEBSITE GITHUB REPO"
-cd ~/Vaults/EC2/websites/ && git add . && git commit -m 'routine cv update' && git push
+# cd ~/Vaults/EC2/websites/mclevey.github.io/ && git add . && git commit -m 'routine cv update' && git push
 
 
 
