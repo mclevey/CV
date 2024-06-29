a8969302db211b2181b254b0c3ae28229c7b7dfe
John McLevey <john.mclevey@hey.com>
2024-06-29 13:08:05 -0230

    routine cv update

--- compile_cv.sh
+++ compile_cv.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-# Define color codes
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-YELLOW_REG='\033[0;33m'
-NC='\033[0m' # No Color
-
-# Print colored messages
-echo -e "${YELLOW}[mstk]${YELLOW_REG} compiling and publishing cv.md${NC}"
-
 set -e
+
+GREEN='\033[0;32m'
+YELLOW_BOLD='\033[1;33m'
+YELLOW_REG='\033[0;33m'
+NOCOLOR='\033[0m'
+
+echo -e "${YELLOW_BOLD}[mstk]${YELLOW_REG} compiling and publishing cv.md${NOCOLOR}"
 mstk pipeline cv .
 
-echo -e "${YELLOW}[mstk]${YELLOW_REG} committing changes to git repo: curriculum-vitae${NC}"
-git add .
-git commit -m "routine cv update"
-git push
+echo -e "${YELLOW_BOLD}[mstk]${YELLOW_REG} committing changes to git repo: curriculum-vitae${NOCOLOR}"
+git add . && git commit -m "routine cv update" && git push
