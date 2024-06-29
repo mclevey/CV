3186f198f6a9ae28e4d669026463766b9a5504c5
John McLevey <john.mclevey@hey.com>
2024-06-29 13:03:45 -0230

    routine cv update

--- compile_cv.sh
+++ compile_cv.sh
@@ -5,6 +5,7 @@ echo -e "[mstk] compiling and publishing cv.md"
 set -e 	
 mstk pipeline cv .
 
+echo -e "[mstk] commiting changes to git repo: curriculum-vitae"
 git add .
 git commit -m "routine cv update"
 git push
